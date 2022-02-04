7dea3dc9c63e89fab82e32647209a38242c7b20f
EnriqueSFlor <enriquesilvaflor@gmail.com>
2022-02-03 22:40:51 -0300

    iniciando novo projeto

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Container from './components/layout/Container';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import Home from './components/pages/Home';
+import NewProject from './components/pages/NewProject';
+
+function App() {
+  return (
+   <Router>
+     <div>
+       <Link to="/">Home</Link>
+       <Link to="/company">Empresa</Link>
+       <Link to="/contact">Contato</Link>
+       <Link to="/newproject">Novo projeto</Link>
+     </div>
+
+     <Container customClass="minHeight">
+     <Routes>
+       <Route exact path="/" element={<Home />}/>
+       <Route exact path="/company" element={ <Company />}/>
+       <Route exact path="/contact" element={<Contact />}/>
+       <Route exact path="/newproject" element={<NewProject />}/>
+     </Routes>
+     </Container>
+     <p>Footer</p>
+
+     
+   </Router>
+  );
+}
+
+export default App;
