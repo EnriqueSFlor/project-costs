68b5955aa3855c7bb15868f70d4698920bae786f
EnriqueSFlor <enriquesilvaflor@gmail.com>
2022-02-22 21:42:37 -0300

    colocando serviços

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -15,6 +15,7 @@ export default function Project(){
     const { id } = useParams()
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
+    const [showServiceForm, setShowServiceForm] = useState(false)
     const [message, setMessage] = useState()
     const [type, setType] = useState()
 
@@ -34,6 +35,7 @@ export default function Project(){
     }, [id])
 
     function editPost(project){
+        setMessage('')
             // budget validation
         if(project.budget < project.cost) {
             setMessage('O orçamento não pode ser menor que o custo do projeto!')
@@ -61,6 +63,11 @@ export default function Project(){
 
     function toggleProjectForm() {
         setShowProjectForm(!showProjectForm)
+    }
+
+    
+    function toggleServiceForm() {
+        setShowServiceForm(!showServiceForm)
     }
     
     return (
@@ -94,6 +101,19 @@ export default function Project(){
                             </div>
                         )}
                     </div>
+                    <div className={styles.service_form_container}>
+                        <h2>Adicione um serviço:</h2>
+                        <button className={styles.btn} onClick={toggleServiceForm}>
+                            {!showServiceForm ? 'Adicionar serviço' : 'Fechar'}
+                        </button>
+                        <div className={styles.project_info}>
+                            {showServiceForm && <div>formulário do serviço</div>}
+                        </div>
+                    </div>
+                    <h2>Serviços</h2>
+                    <Container customClass="start">
+                            <p>Itens de serviços</p>
+                    </Container>
                    </Container>
                </div>
             ): (
